45ddf632a9cc50230307ecbee399d392b5a5888e
Miladul <imiladul@gmail.com>
2020-10-12 00:40:39 +0600

    First git Initialized

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>LMS</title>
+
+	<!------Bootstrap v3.3.6------>
+	<link rel="stylesheet" type="text/css" href="assets/vendor/bootstrap/css/bootstrap.css">
+</head>
+<body>
+	<div class="container">
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<div class="row">
+			<div class="col-sm-4 col-sm-offset-4">
+				<a class="btn btn-primary btn-block" href="student">Student</a>
+				<a class="btn btn-primary btn-block" href="librarian">Librarian</a>
+				
+			</div>
+		</div>
+		 
+	</div>
+</body>
+</html>
