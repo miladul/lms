50b52ec26665d56328aaee26a7856ea9733c74dc
Miladul <imiladul@gmail.com>
2020-10-29 00:53:04 +0600

    Data inser to issue book table from form

--- librarian/issue-book.php
+++ librarian/issue-book.php
@@ -20,6 +20,13 @@
 				<div class="panel-content">
 					<div class="row">
 						<div class="col-md-12">
+							<?php
+							if(isset($_GET['issued-book'])){ ?>
+								<div class="alert alert-success alert-dismissable">
+									<a href="javascript:void(0)" class="close" data-dismiss="alert" aria-label="close">×</a>
+									<strong>Book issued successfully </strong>
+								</div>
+							<?php } ?>
 							<form class="form-inline" method="POST" action="">
 								<h5>Issue Book</h5>
 								<div class="form-group">
@@ -42,52 +49,46 @@
 
 								if($count_student==1){
 									if($row['status']==1){ ?>
-										<form class="form-inline" method="POST" action="">
-											<table class="table table-bodered">
-												<tr>
-													<th>Student Name: </th>
-													<td><?=$row['name']?></td>
-												</tr>
-												<tr>
-													<th>Student Roll: </th>
-													<td><?=$row['roll']?></td>
-												</tr>
-												<tr>
-													<th>Registration No: </th>
-													<td><?=$row['reg']?></td>
-												</tr>
-												<tr>
-													<th>Select a Book: </th>
-													<td>
-														<select name="book_id" class="form-control">
-															<option>Choose a book</option>
-															<?php
-															$books = mysqli_query($link, "SELECT * FROM `books` WHERE `status`=1");
-															while($rows = mysqli_fetch_assoc($books)){ ?>
-																<option value="<?= $rows['id']?>"><?= $rows['book_name']?></option>
-															<?php } ?>
-														</select>
 
-													</td>
-												</tr>
-											</table>
-											<div class="row">
-												<div class="col-sm-6 col-sm-offset-6">
-													<div class="form-group">
-													<div class="">
-														<button class="btn btn-primary" name="issue_book"><i class="fa fa-send"></i>&nbsp; Issue Book</button>
+										<div class="panel">
+											<div class="panel-content">
+												<div class="row">
+													<div class="col-md-12">
+														<form action="" method="POST">
+															<div class="form-group">
+																<input type="hidden" name="student_id" value="<?=$row['id']?>">
+																<label for="name">Student Name</label>
+																<input type="text" readonly="" class="form-control" id="book_name" name="name" value="<?=$row['name']?>">
+															</div>
+															<div class="form-group">
+																<label for="name">Book Name</label>
+																<select name="book_id" class="form-control">
+																	<option value="0">Choose a book</option>
+																	<?php
+																	$books = mysqli_query($link, "SELECT * FROM `books` WHERE `status`=1");
+																	while($rows = mysqli_fetch_assoc($books)){ ?>
+																		<option value="<?= $rows['id']?>"><?= $rows['book_name']?></option>
+																	<?php } ?>
+																</select>
+															</div>
+															<div class="form-group">
+																<label for="book_issue_date">Book Issue Date</label>
+																<input type="date" readonly="" class="form-control" id="book_issue_date" name="book_issue_date" value="<?= date("Y-m-d") ?>">
+															</div>
+															<div class="form-group">
+																<button type="submit" name="issue_book" class="btn btn-primary">Issue Book</button>
+															</div>
+
+														</form>
 													</div>
 												</div>
-												</div>
 											</div>
-										</form>
+										</div>
 										<?php
 
 									}else{
 										$error = "This student is inactive";
 									} ?>
-
-									
 
 									<?php
 
@@ -95,24 +96,58 @@
 									$error = "Student not found";
 								}
 								?>
-
 								<?php
 								if(isset($error)){ ?>
-									<div class="alert alert-danger alert-dismissable">
+									<div class="alert alert-warning alert-dismissable">
 										<a href="javascript:void(0)" class="close" data-dismiss="alert" aria-label="close">×</a>
 										<strong><?= $error ?></strong>
 									</div>
-									
 								<?php } ?>
 							<?php } ?>
 
-						</div> 
-
+						</div>
 					</div>
+					<?php
+					if(isset($_POST['issue_book'])){
+						
+
+						$student_id = $_POST['student_id'];
+						$book_id = $_POST['book_id'];
+						$book_issue_date = $_POST['book_issue_date'];
+
+						$check_already_issued = mysqli_query($link, "SELECT * FROM `issue_books` WHERE `student_id`='$student_id' AND `book_id`='$book_id' ");
+						$count_issue_book = mysqli_num_rows($check_already_issued);
+						if($count_issue_book==0){
+							$inser_book_issue = mysqli_query($link, "INSERT INTO `issue_books`(`student_id`, `book_id`, `book_issue_date`) VALUES ('$student_id','$book_id','$book_issue_date')");
+							if($inser_book_issue){
+								header('location: issue-book.php?issued-book');
+							}
+						}else{
+							$already_issued_error = "Already issued this book for this student";
+						}
+					}
+
+					?>
+
+					<?php
+					if(isset($already_issued_error)){ ?>
+						<div class="alert alert-warning alert-dismissable">
+							<a href="javascript:void(0)" class="close" data-dismiss="alert" aria-label="close">×</a>
+							<strong><?= $already_issued_error ?></strong>
+						</div>
+					<?php } ?>
+
 				</div>
 			</div> 
 		</div>
 	</div>
+
+
+
+
+
+
+
 </div>
 
 <?php require_once('footer.php'); ?>
